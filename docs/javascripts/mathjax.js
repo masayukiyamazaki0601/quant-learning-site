3b3c61bd357eac45a134142ef678225d88595157
masayuki <masayuki@local>
2026-09-05 11:50:08 +0900

    MkDocs Material 化: 52ユニットの下書き生成とロードマップMarkdown化

---
+++ docs/javascripts/mathjax.js
@@ -0,0 +1,11 @@
+window.MathJax = {
+  tex: {
+    inlineMath: [["\\(", "\\)"]],
+    displayMath: [["\\[", "\\]"]],
+    processEscapes: true
+  },
+  options: {
+    ignoreHtmlClass: "tex2jax_ignore|mathjax_ignore|md-nav",
+    processHtmlClass: "tex2jax_process|mathjax_process|arithmatex"
+  }
+};
